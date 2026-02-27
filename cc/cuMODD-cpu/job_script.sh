f469808416daf739f5e9e6df3707da3b79b616c9
Rahul Mihir Patel <rahulpat@l4.nibi.sharcnet>
2026-02-27 13:38:39 -0500

    Add CPU farm

---
+++ cc/cuMODD-cpu/job_script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#  You have to replace Your_account_name below with the name of your account:
+#SBATCH -A Your_account_name
+
+# Here you should provide the sbatch arguments to be used in all jobs in this farm
+# At the very least, it has to contain the runtime switch (either -t or --time):
+#SBATCH -t 0-00:20
+#SBATCH --mem=17000M
+
+# If WHOLE_NODE=1 in config.h file, the following sbatch arguments will be automatically added:
+# --nodes=1 --cpus-per-task=$NWHOLE --exclusive , where $NWHOLE is also defined in config.h
+
+
+# Don't change the lines below
+#=====================================================================
+
+module load meta-farm
+
+if (($WHOLE_NODE==1))
+ then
+ for ((i=0; i<$SLURM_CPUS_PER_TASK; i++))
+  do
+  task.run &
+  done
+ wait
+
+ else
+ task.run
+
+ fi
