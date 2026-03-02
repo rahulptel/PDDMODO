c8a1c7c0cb4309be9afced6d66bd0762b8673f4c
rahulptel <rahul.polymtl@gmail.com>
2026-03-02 18:44:45 -0500

    Update job script

--- cc/cuMODD-cpu-8/job_script.sh
+++ cc/cuMODD-cpu-8/job_script.sh
@@ -3,7 +3,7 @@
 # Here you should provide the sbatch arguments to be used in all jobs in this farm
 # At the very least, it has to contain the runtime switch (either -t or --time):
 #SBATCH -t 0-03:00
-#SBATCH --mem=17000M
+#SBATCH --mem=16.5G
 #SBATCH --cpus-per-task=8
 
 # If WHOLE_NODE=1 in config.h file, the following sbatch arguments will be automatically added:
